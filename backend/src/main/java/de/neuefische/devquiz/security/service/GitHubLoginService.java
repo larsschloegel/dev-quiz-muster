dfeac64a760403644ec6d4662bb9aebfc674bf44
Lars Schlögel <lars.schloegel@gmail.com>
2021-10-28 12:51:43 +0200

    - Create JWT access token

--- backend/src/main/java/de/neuefische/devquiz/security/service/GitHubLoginService.java
+++ backend/src/main/java/de/neuefische/devquiz/security/service/GitHubLoginService.java
@@ -5,13 +5,17 @@ import de.neuefische.devquiz.security.api.GitHubApiService;
 import de.neuefische.devquiz.security.model.GitHubUserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Service
 public class GitHubLoginService {
 
     private final GitHubApiService gitHubApiService;
+    private final JWTUtilService jwtUtilService;
 
-    public GitHubLoginService(GitHubApiService gitHubApiService) {
+    public GitHubLoginService(GitHubApiService gitHubApiService, JWTUtilService jwtUtilService) {
         this.gitHubApiService = gitHubApiService;
+        this.jwtUtilService = jwtUtilService;
     }
 
     public String verifyGitHubCode(String code) {
@@ -22,7 +26,6 @@ public class GitHubLoginService {
         GitHubUserDto gitHubUserDto = gitHubApiService.retrieveUserInfo(gitHubToken);
 
         //Create JWT access token
-
-        return "null";
+        return jwtUtilService.createToken(new HashMap<>(), gitHubUserDto.getLogin());
     }
 }
